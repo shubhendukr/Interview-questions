108eef5df11e8b540675746086b9d53839b08580
Shubhendu Kumar <34783719+shubhendukr@users.noreply.github.com>
2018-04-04 20:48:40 +0800

    Run length encoding

---
+++ CompressString.java
@@ -0,0 +1,45 @@
+/*
+Run Length Encoding
+-----------------------
+Given an input string, write a function that 
+returns the Run Length Encoded string for the input string.
+
+Enter string: aabbcdeee
+Input: aabbcdeee
+Compressed: a2b2cde3
+*/
+
+import java.util.Scanner;
+ 
+public class CompressString {
+   public static void main(String[] args) {
+ 
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter string: ");
+      String str = sc.nextLine();
+      System.out.println("Input: " + str);
+ 
+      String compressed = "";
+ 
+      char ch=0;
+      int count=1;
+      for (int x = 0; x < str.length(); x++) { 
+         if (ch == str.charAt(x)){
+            count = count + 1;
+         } else {
+            compressed = compressed + ch;
+            System.out.println(compressed);
+            if(count != 1){
+               compressed = compressed + count;
+            }
+            ch = str.charAt(x);
+            count = 1;
+         }
+      }
+      compressed = compressed + ch;
+      if(count != 1){
+         compressed = compressed + count;
+      }
+      System.out.println("Compressed: " + compressed); 
+   }
+}
