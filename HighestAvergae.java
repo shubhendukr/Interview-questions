6579ca9d34f5d9b28b15d39b6932a920278ed9de
Shubhendu Kumar <34783719+shubhendukr@users.noreply.github.com>
2019-08-14 14:38:45 +0800

    Optimizing the Hashmap entry

--- HighestAvergae.java
+++ HighestAvergae.java
@@ -1,24 +1,19 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class HighestAverage {
 	public static void main(String[] args) {
-		String[][] scores = {{"A", "80"}, {"B", "84"}, {"A", "88"}, {"C", "90"}, {"B", "97"}, {"C", "89"}};
+		String[][] scores = {{"John", "80"}, {"Derek", "84"}, {"John", "88"}, {"Cynthia", "90"}, {"Derek", "97"}, {"Cynthia", "89"}};
 		HashMap<String, List<Integer>> scoreMap = new HashMap<>();
 		double highestAvg = 0.0;
 		String studentName = "";
 		for(int i=0; i<scores.length; i++) {
-			if(scoreMap.containsKey(scores[i][0])) {
-				List<Integer> marks = scoreMap.get(scores[i][0]);
-				marks.add(Integer.valueOf(scores[i][1]));
-				scoreMap.put(scores[i][0], marks);
-			} else {
-				List<Integer> marks = new ArrayList<>(Arrays.asList(Integer.valueOf(scores[i][1])));
-				scoreMap.put(scores[i][0], marks);
+			if (!scoreMap.containsKey(scores[i][0])) {
+				scoreMap.put(scores[i][0], new ArrayList<>());
 			}
+			scoreMap.get(scores[i][0]).add(Integer.valueOf(scores[i][1]));
 		}
 		
 		for(Map.Entry<String, List<Integer>> entry : scoreMap.entrySet()) {
@@ -28,8 +23,6 @@ public class HighestAverage {
 				studentName = entry.getKey();
 			}
 		}
-		System.out.println(scoreMap);
-		System.out.println("Student Name: "+studentName+" with highest Avg: "+highestAvg);
+		System.out.println("Student Name: "+studentName+" with highest Avg "+highestAvg);
 	}
-
 }
